b840d8b1568f3c1126501f29c797662aed030914
Susie Coleman <susie.coleman@guardian.co.uk>
2017-07-19 17:46:58 +0100

    Add environment credentials for lambda.

--- src/main/scala/metricsLambdas/Config.scala
+++ src/main/scala/metricsLambdas/Config.scala
@@ -1,6 +1,6 @@
 package metricsLambdas
 
-import com.amazonaws.auth.{AWSCredentialsProviderChain, InstanceProfileCredentialsProvider}
+import com.amazonaws.auth.{AWSCredentialsProviderChain, EnvironmentVariableCredentialsProvider, InstanceProfileCredentialsProvider}
 import com.amazonaws.auth.profile.ProfileCredentialsProvider
 import com.amazonaws.regions.Regions
 
@@ -9,6 +9,7 @@ object Config {
   val region = Option(System.getenv("AWS_DEFAULT_REGION")).map(Regions.fromName).get
 
   val awsCredentialsProvider = new AWSCredentialsProviderChain(
+    new EnvironmentVariableCredentialsProvider(),
     new ProfileCredentialsProvider("composer"),
     new InstanceProfileCredentialsProvider(false)
   )
